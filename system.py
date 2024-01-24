4d4514b799d53bc798b3b5a12460dac375454537
Mateusz <mateusz@example.pl>
2024-01-24 15:42:00 +0100

    Nie wiem o co chodzi

--- system.py
+++ system.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
 
-import ipaddress
-
-class Network():
-    pass
-
-
 class Device():
 
-    def __init__(self, ID: int, gns_id: str, name: str):
-        self.ID = ID
+    _ipv4_address_pull = [f"192.168.10.{x}" for x in range(11, 40)]
+
+    def __init__(self, dev_id: int, gns_id: str, name: str):
+        self.dev_id = dev_id
         self.gns_id = gns_id
         self.name = name
-    
+
     @property
-    def ip_mgmnt(self, network):
-        Network
+    def ip_mngt(self):
+        ip = Device._ipv4_address_pull[0]
+        Device._ipv4_address_pull.remove(ip)
+        return ip
+
+    
+
 def main():
-    pass
+    dev = Device(923487523, "shdlfkzxcv", "R1")
+    print(dev.dev_id)
+    print(dev.ip_mngt)
 
 if __name__ == "__main__":
     main()
