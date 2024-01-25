e2feb42bcd6b785b9a7b3db99b2a8fac35cbb2f5
Mateusz <mateusz@example.pl>
2024-01-25 17:45:49 +0100

    Add Network class. Trying to start this code.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from gns_data import get_json_files, show_in_file
 from other import nice_print
-from my_system import Device, create_devObj
+from my_system import Device, create_system, Network
 from connection import basic_config, Telnet_Conn
 import time
 
@@ -15,9 +15,9 @@ def main():
     dct_nodes, dct_links = get_json_files(path)
 
     ## Create device with information from lab.
-    create_devObj(dct_nodes)
+    create_system(dct_nodes, dct_links)
     lst_devObj = Device.dev_lst
-    print(lst_devObj)
+    # print(lst_devObj[0].get_links())
     
     # basic_config(lst_devObj[0])
     
--- my_system.py
+++ my_system.py
@@ -2,21 +2,19 @@
 
 import time
 
-class Device():
+
+
+class Network():
+
 
     ipv4_addresses_pool = [f"192.168.10.{x}" for x in range(11, 40)]
     used_addresses = []
 
-    dev_lst = []
 
-    def __init__(self, dev_id: int, gns_id: str, name: str, console_port: int):
-        self.dev_id = dev_id
-        self.gns_id = gns_id
-        self.name = name
-        self.console_port = console_port
-        self.vendor = None
-        self.ip_mgmt = None
-        Device.dev_lst.append(self)
+    def __init__(self, links):
+        self.links = links
+        print(self.links)
+
 
     @classmethod
     def get_ip_address(cls, vendor):
@@ -25,39 +23,93 @@ class Device():
         cls.ipv4_addresses_pool.remove(ip)
         return ip
 
+
+    def connection_create(self, gns_id):
+        print(gns_id)
+        # for link in self.links:
+        #     if gns_id == link[1][1] or gns_id == link[2][1]:
+        #         return link
+
+
+
+class Device():
+
+    dev_lst = []
+
+
+    def __init__(
+            self,
+            network: object,
+            dev_id: int,
+            gns_id: str,
+            name: str,
+            console_port: int
+            ):
+        self.network = network
+        self.dev_id = dev_id
+        self.gns_id = gns_id
+        self.name = name
+        self.console_port = console_port
+        self.vendor = None
+        self.ip_mgmt = None
+        self.links = None
+        Device.dev_lst.append(self)
+
+
     @staticmethod
     def show_used_addresses():
         return Device.used_addresses
 
+
     @staticmethod
     def show_free_addresses():
         return Device.ipv4_addresses_pool
-    
+
+
+    def get_links(self):
+        print(self.gns_id)
+        return Network.connection_create(self.gns_id)
+
+
 
 class IOS(Device):
 
-    def __init__(self, dev_id: int, gns_id: str, name: str, console_port: int):
-        super().__init__(dev_id, gns_id, name, console_port)
+
+    def __init__(
+            self,
+            network: object,
+            dev_id: int,
+            gns_id: str,
+            name: str,
+            console_port: int
+            ):
+        super().__init__(network, dev_id, gns_id, name, console_port)
         self.vendor = "vIOS"
         self.username = "cisco"
         self.password = "cisco"
-        self.ip_mgmt = Device.get_ip_address(self)
+        self.ip_mgmt = Network.get_ip_address(self)
 
 
 
-def create_devObj(dct):
+def create_system(nodes, links):
+    network = Network(links)
     nodes_lst = []
-    for dev in dct:
+
+    for dev in nodes:
         dev_id = dev[0]
         gns_id = dev[1][0]
         console_port = dev[1][1]
         name = dev[1][2]
-        if "vIOS" is dev[1][3]:
-            node = IOS(dev_id, gns_id, name, console_port)
+
+        if "vIOS" == dev[1][3]:
+            node = IOS(network, dev_id, gns_id, name, console_port)
+
         else:
-            node = Device(dev_id, gns_id, name, console_port)
+            node = Device(network, dev_id, gns_id, name, console_port)
         nodes_lst.append(node)
+
     return nodes_lst
+
 
 
 if __name__ == "__main__":
