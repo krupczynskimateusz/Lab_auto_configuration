3c50dd96af78660d9fd41a7e7ad36afb1f6ae041
Mateusz <mateusz@example.pl>
2024-01-29 16:23:44 +0100

    Some function

--- cli_menu.py
+++ cli_menu.py
@@ -6,15 +6,14 @@ from time import sleep
 
 def show_menu():
     tmp = 0 
-    while tmp < 10:
-        print(f"""{"#" * 20}
-Auto konfiguration menu:
-
-{"#" * 20}
-""")
-
-
-
+    option_lst = ["Start", "Chose path to lab configuration.", "Chose option to konfiguration.", "Enter configuration for managment network.", "Exit"]
+    while tmp < 2:
+        print("#" * 20)
+        print("KONFIGURATION MENU")
+        for index, option in enumerate(option_lst, start = 1):
+            print(f"{index:02}. {option}")
+        chosed_option = input("Chose option number: ")
+        if chosed_option == ""
         tmp += 1
 
 
