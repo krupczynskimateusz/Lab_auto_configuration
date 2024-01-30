055f20f9d25ddc0612ea8b3cd270b069b835ce15
Mateusz <mateusz@example.pl>
2024-01-30 11:33:18 +0100

    Cleaning and preparation

--- main.py
+++ main.py
@@ -10,42 +10,26 @@ import time
 
 def main():
     
-    ## Get info about links and nodes in lab.
-    path = "gns3_file/automation_test2.gns3"
-    dct_nodes, dct_links = get_json_files(path)
-    # show_in_file(dct_nodes, "file/dct_nodes2.json")
-    # show_in_file(dct_links, "file/dct_links2.json")
+    # ## Get info about links and nodes in lab.
+    # path = "gns3_file/automation_test2.gns3"
+    # dct_nodes, dct_links = get_json_files(path)
+    # # show_in_file(dct_nodes, "file/dct_nodes2.json")
+    # # show_in_file(dct_links, "file/dct_links2.json")
 
-    ## Create device with information from lab.
-    create_system(dct_nodes, dct_links)
+    # ## Create device with information from lab.
+    # create_system(dct_nodes, dct_links)=
 
-    print("\n")
-    print("#" * 20)
-    print("\n")
+    # print("\n")
+    # print("#" * 20)
+    # print("\n")
 
-    # for dev in Device.dev_lst[3:6]:
-
-    #     if dev.vendor != None:
-    #         dev_conf = commands.create_config_obj(dev) ## -> connection.py
-    #         print(dev_conf.basic_config())
-    #         print(dev_conf.ssh_config())
-    #         print(dev_conf.create_ip_mgmt())
-    #         print(dev_conf.create_config_interface())
-    #         print("\n")
-    #         print("#" * 20)
-    #         print("\n")
-
-    #     else:
-    #         pass
-
-
-    for dev in Device.dev_lst:
-        print(dev.name)
-        upload_basic_config(dev)
-        print("\n")
-        print("#" * 20)
-        print("\n")
-
+    # for dev in Device.dev_lst:
+    #     print(dev.name)
+    #     upload_basic_config(dev)
+    #     print("\n")
+    #     print("#" * 20)
+    #     print("\n")
+    pass
 
 
 if __name__ == "__main__":
