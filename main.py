a743333c86355dbcdb5b085da7068e4b797577e0
Mateusz <mateusz@example.pl>
2024-02-01 17:16:22 +0100

    Some cleaing

--- main.py
+++ main.py
@@ -128,30 +128,5 @@ def main():
             sleep(1)
 
 
-
-############################################################
-    
-    # ## Get info about links and nodes in lab.
-    # path = "gns3_file/automation_test2.gns3"
-    # dct_nodes, dct_links = get_json_files(path)
-    # # show_in_file(dct_nodes, "file/dct_nodes2.json")
-    # # show_in_file(dct_links, "file/dct_links2.json")
-
-    # # ## Create device with information from lab.
-    # create_system(dct_nodes, dct_links)
-
-    # print("\n")
-    # print("#" * 20)
-    # print("\n")
-
-    # for dev in Device.dev_lst:
-    #     print(dev.name)
-    #     upload_basic_config(dev)
-    #     print("\n")
-    #     print("#" * 20)
-    #     print("\n")
-    # pass
-
-
 if __name__ == "__main__":
     main()
